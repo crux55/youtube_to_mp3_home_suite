f974ccdb5d92e8cf7d1047ef24f3ae2de0b9fe6a
Andrew Quinn <code@andruquinn.com>
2026-06-08 12:12:06 +1000

    fix: use ios player client to bypass SABR streaming

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ ydl_opts = {
     'skip_unavailable_fragments': True,
     'extractor_retries': 3,
     'http_chunk_size': 10485760,  # 10MB chunks
+    'extractor_args': {'youtube': {'player_client': ['ios']}},
     }
 
 
